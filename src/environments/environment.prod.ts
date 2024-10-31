5cf11f53ec610747b85f98fed8a2205d179a949e
nicolas.gonzalez <g.nicolas@bue.edu.ar>
2024-10-31 18:08:49 -0300

    cambios y compilacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDSAHlcbU-42JtKq_MDTBY9EtZD47oLdec",
+    authDomain: "climas-app.firebaseapp.com",
+    projectId: "climas-app",
+    storageBucket: "climas-app.appspot.com",
+    messagingSenderId: "364451586110",
+    appId: "1:364451586110:web:e32bc2314cbb635066716f",
+    measurementId: "G-RFXKDKKZB9"
+  }
 };
